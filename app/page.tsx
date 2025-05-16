c488c977e03ff3519627ae256386751743633438
Geraldiene <riomahabramromamateo@gmail.com>
2025-05-16 08:45:18 -0400

    componentes y pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import { NavigationMenu } from "@radix-ui/react-navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>Dasboard</NavigationMenuItem>
+          <NavigationMenuItem>Historico</NavigationMenuItem>
+          <NavigationMenuItem>Reportes</NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
     </div>
   );
 }
