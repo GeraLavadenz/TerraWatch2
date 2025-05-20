b7ec4681b38bd4d77f7e82daffc4ab7e0a38a6a0
Geraldiene <riomahabramromamateo@gmail.com>
2025-05-20 10:24:32 -0400

    new component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Menu from '@/components/Menu';
 import Header from '@/components/header';
 import { AlertSection } from '@/components/alertSection';
-
+ 
 function Dashboard() {
   return (
     <div>
