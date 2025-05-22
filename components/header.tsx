d1317c832ca4b4f718eebd55652335f5ebacd6e6
ro-io <huanapacowara@gmail.com>
2025-05-22 05:00:11 -0400

    fondo

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,23 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <div className="fixed z-50 w-full h-16 flex justify-center items-center border-b border-gray-300 dark:border-gray-700 shadow-sm bg-white dark:bg-[#121212]">
-        <header className="w-full h-16 flex  items-center space-x-4">
+    <div className="relative z-50 w-full h-16 border-b border-gray-300 dark:border-gray-700 shadow-sm bg-white dark:bg-[#121212] overflow-hidden">
+      
+      {/* Fondo de imagen con opacidad */}
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-15 pointer-events-none"
+        style={{ backgroundImage: `url('/image/fondo-header.png')` }} // cambia el path por el correcto
+      />
+
+      {/* Contenido del header */}
+      <header className="relative w-full h-16 flex items-center px-4 space-x-4 z-10">
         <Image 
-        src="/image/agricultura.png"
-        alt='logo'
-        width={80}
-        height={50}        
+          src="/image/agricultura.png"
+          alt="logo"
+          width={80}
+          height={50}        
         />
-        <h1  className="text-2xl font-semibold text-500 tracking-wide">TerraNova</h1>
+        <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100 tracking-wide">TerraWatch</h1>
       </header>
     </div>  
   )
