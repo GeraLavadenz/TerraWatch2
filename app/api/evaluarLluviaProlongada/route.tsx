0b77618ed5a85fab54187310fd842d294c7fc7ee
Geraldiene Lavadenz <riomahabramromamateo@gmail.com>
2025-06-12 23:38:32 -0400

    `Removed type property from serviceAccount object`

--- app/api/evaluarLluviaProlongada/route.tsx
+++ app/api/evaluarLluviaProlongada/route.tsx
@@ -11,7 +11,6 @@ interface LecturaSensor {
 
 // ✅ Configurar credenciales desde variables de entorno
 const serviceAccount: ServiceAccount = {
-  type: "service_account",
   project_id: process.env.FB_PROJECT_ID!,
   private_key_id: process.env.FB_PRIVATE_KEY_ID!,
   private_key: process.env.FB_PRIVATE_KEY!.replace(/\\n/g, "\n"),
