57b7dfeef9b722861270177f00591c6af251e1f1
Geraldiene Lavadenz <riomahabramromamateo@gmail.com>
2025-06-12 23:49:54 -0400

    `Added dotenv package and updated package-lock.json and package.json accordingly`

--- app/api/evaluarLluviaProlongada/route.tsx
+++ app/api/evaluarLluviaProlongada/route.tsx
@@ -11,16 +11,9 @@ interface LecturaSensor {
 
 // ✅ Configurar credenciales desde variables de entorno
 const serviceAccount: ServiceAccount = {
-  project_id: process.env.FB_PROJECT_ID!,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID!,
-  private_key: process.env.FB_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-  client_email: process.env.FB_CLIENT_EMAIL!,
-  client_id: process.env.FB_CLIENT_ID!,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL!,
-  universe_domain: "googleapis.com",
+  projectId: process.env.FB_PROJECT_ID!,
+  clientEmail: process.env.FB_CLIENT_EMAIL!,
+  privateKey: process.env.FB_PRIVATE_KEY!.replace(/\\n/g, "\n"),
 };
 
 // ✅ Inicializar Firebase si no está ya inicializado
